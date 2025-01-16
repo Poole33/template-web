e532ace8367a5c2c70ecfc46193af382bf7570e4
Poole <ptcp3@hotmail.com>
2025-01-16 17:05:06 +0800

    优化打包去出console

--- vite.config.js
+++ vite.config.js
@@ -6,27 +6,30 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    UnoCSS({
-        configFile: './uno.config.js'
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 6666,
-    proxy: {
-        '/backend/api': {
-            target: 'http://10.10.24.127:8888',
-            // target: 'http://10.10.143.51:8080',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/backend/, ''),
+    esbuild:{
+        drop: ['console', 'debugger'],
+    },
+    plugins: [
+        vue(),
+        UnoCSS({
+            configFile: './uno.config.js'
+        }),
+    ],
+    resolve: {
+        alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 6666,
+        proxy: {
+            '/backend/api': {
+                target: 'http://10.10.24.127:8888',
+                // target: 'http://10.10.143.51:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/backend/, ''),
+            }
         }
     }
-  }
 })
